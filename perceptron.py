fcfac62ffb37a10ffa61e136e90bf46e422553f1
Mirror83 <glenochieng045@gmail.com>
2025-02-13 09:28:21 +0300

    Initial commit

---
+++ perceptron.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Perceptron:
+    def __init__(self, inputs: List[int], weights: List[int]):
+        if len(inputs) != len(weights):
+            raise Exception(
+                "The no of weights should be equal to number of inputs"
+            )
+        self.inputs = inputs
+        self.weights = weights
+        self.bias = -5
+
+    def output(self):
+        summation = sum(weight * input for weight, input in zip(self.weights, self.inputs))
+        if summation + self.bias <= 0:
+            return 0
+        else:
+            return 1
+
+weather_good, partner_coming, transport_av = 1, 0, 1
+input = [weather_good, partner_coming, transport_av]
+
+perc = Perceptron(input, [2,6,2])
+print(perc.output())
